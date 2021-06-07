353f29f9512017e2237496e587249992cfd1b76b
tanmaythaware <85193272+tanmaythaware@users.noreply.github.com>
2021-06-07 17:00:58 +0530

    Add files via upload

---
+++ AreaCalculator/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Area of Circle is " + AreaCalculator.area(5));
+        System.out.println("Area of Rectangle is " + AreaCalculator.area(5,4));
+    }
+}
